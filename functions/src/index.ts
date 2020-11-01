b18b36c97db466bd380e99388293effe6b0d1606
PasinduDew <pasindudewapriya96@gmail.com>
2020-11-01 22:26:50 +0530

    Configured Cloud Functions By adding Express and Other Essential Packages

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,40 @@
+// Import Libraries
 import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as cors from "cors";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+//initialize express server
+const app = express();
+const main = express();
+
+// The Firebase Admin SDK to access Cloud Firestore.
+admin.initializeApp(functions.config().firebase);
+
+//initialize the database and the collection
+// const db = admin.firestore();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+// Middleware that transforms the raw string of req.body into json
+main.use('/v1', app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+// ###########################################################################################################################################
+//                                                  REST End points - Functions
+// ###########################################################################################################################################
+
+// Invoke -> https://us-central1-ucsc-e-procurement.cloudfunctions.net/api/test
+app.get("/test", cors(), (req, res) => {
+  
+    res.send("Ayubowan!, From Cloud Functions. I'm Working :)");
+
 });
+
+
+export const api = functions.https.onRequest(main);
